22b9b4ba8b356c6cba261bb7df6fbe7c0ef3976a
AlejandraMogollon <alejandrame1505@gmail.com>
2022-07-28 11:27:03 -0400

    logo y vista mesas con ruta

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LogoVerden from '../img/Verdenlogo.png'
+
+const Logo = () => {
+  return (
+    <div>
+        <img src={LogoVerden} className='logoverden' />
+    </div>
+  )
+}
+
+export default Logo
