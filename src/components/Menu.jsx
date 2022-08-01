1f5403d8ecf43c7baf221bc6e804e761c6793a04
unknown <mariannybatista2003@gmail.com>
2022-08-01 12:36:03 -0400

    Menu CSS count

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,44 +1,73 @@
 import React from 'react'
 import menudata from '../menudata.json'
+import Tables from './Tables'
+import { useState } from 'react'
 
 const Menu = () => {
 
-    const drinks = menudata.filter((food)=>{
+    const drinks = menudata.filter((food) => {
         return food.type === 'drink'
     })
-    const breakfasts = menudata.filter((food)=>{
+    const breakfasts = menudata.filter((food) => {
         return food.type === 'breakfast'
     })
-    const lunchs = menudata.filter((food)=>{
+    const lunchs = menudata.filter((food) => {
         return food.type === 'lunch'
     })
 
+    // CONTADOR
+    const [count, setCount] = useState(0)
+
     return (
-        <div className='AllConteinerMenu'>
-            <h1>Bebidas</h1>
-            {drinks.map(food => {
-                return  <div> 
-         
-                    <li>{food.name}</li>
-                </div>  
-            })}
-            <h1>Desayunos</h1>
-            {breakfasts.map(food => {
-                return  <div> 
-         
-                    <li>{food.name}</li>
-                </div>  
-            })}
-            <h1>Otros Platos</h1>
-            {lunchs.map(food => {
-                return  <div> 
-         
-                    <li>{food.name}</li>
-                </div>  
-            })}
-            
-            
-        </div>
+        <main className='AllContainerMenu'>
+            <div className='textMenuMenu'>MENU</div>
+            <div className='textTablesMenu'>Mesa no se cuanto</div>
+            <div className='containerMenu'>
+                <li className='textTypesMenu'>Bebidas</li>
+                <div className='containerOptionsMenu'>
+                    {drinks.map(food => {
+                        return <div className='containerPriceFoodCountMenu'>
+                            <div className='textFoodNameMenu'>{food.name}</div>
+                            <div key={food.id} className='textFoodNameMenu'>{food.price}</div>
+                            <button className='buttonCountMenu'>-</button>
+                            <spam className='textFoodNameMenu'>{count}</spam>
+                            <button className='buttonCountMenu' key={food.id} onClick={() => {
+                                if (food.id === food.id) {
+                                    setCount(count + 1)
+                                }
+                            }}>+</button>
+                        </div>
+                    })}
+                </div>
+                <li className='textTypesMenu'>Desayunos</li>
+                <div className='containerOptionsMenu'>
+                    {breakfasts.map(food => {
+                        return <div className='containerPriceFoodCountMenu'>
+                            <div className='textFoodNameMenu'>{food.name}</div>
+                            <div className='textFoodNameMenu'>{food.price}</div>
+                            <button className='buttonCountMenu'>-</button>
+                            <spam className='textFoodNameMenu'>0</spam>
+                            <button className='buttonCountMenu'>+</button>
+                        </div>
+                    })}
+                </div>
+                <li className='textTypesMenu'>Otros Platos</li>
+                <div className='containerOptionsMenu'>
+                    {lunchs.map(food => {
+                        return <div className='containerPriceFoodCountMenu'>
+                            <div className='textFoodNameMenu'>{food.name}</div>
+                            <div className='textFoodNameMenu'>{food.price}</div>
+                            <button className='buttonCountMenu'>-</button>
+                            <spam className='textFoodNameMenu'>0</spam>
+                            <button className='buttonCountMenu'>+</button>
+                        </div>
+                    })}
+                </div>
+            </div>
+            <div className='buttonNextMenu'>
+                <button className="btn mrg-2">SIGUIENTE</button>
+            </div>
+        </main>
     )
 }
 
