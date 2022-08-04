24623925c5ffd7effa248ba61fe1cdceb5fe2b38
unknown <mariannybatista2003@gmail.com>
2022-08-04 11:51:05 -0400

    Vista Order casi lista (tiene subtotal y CSS culminado)

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,25 +1,52 @@
 import React from 'react'
 
 
-const Orders = ({orders, tableRegister}) => {
+const Orders = ({ orders, tableRegister }) => {
 
-// console.log('orders en orders', orders)
+  // console.log('orders en orders', orders)
+
+  const totalPrice = () => {
+    let count = 0;
+
+    orders.map((food) => {
+      count += food.price * food.count
+    })
+    return count
+  }
 
   return (
-    <div>
-       {orders.map((food, index) => {
-        console.log('aqui estoy',food)
+    <div className='container'>
+      <div className='containerAllOrder'>
+        <div className='containerTextOrderWithTable'>
+          <h1 className='textOrderOrders'>PEDIDO</h1>
+          <h2>Mesa #{tableRegister}</h2>
+          <h4>Confirmar para cocinar</h4>
+        </div>
+        <div className="containerAlltextOrderItems">
+          {orders.map((food, index) => {
+            console.log('aqui estoy', food)
             return (
+
               <div key={index}>
-                <div>Mesa # {tableRegister}</div>
-                <div>{food.name}</div>
-                <div>{food.count}</div>
-                <div>{food.price}</div>
+                <div className='containertextOrderItems'>
+                  <div>{food.name}</div>
+                  <div>{food.count}</div>
+                  <div>{food.price}$</div>
+                </div>
               </div>
             );
           })}
-          <div><button>Enviar Pedido</button></div>
+        </div>
+        <div className="textTotalOrder">
+          <p>Total: {totalPrice()}$</p>
+        </div>
+      </div>
+      <div className='containerAllButtonsOrder'>
+        <button class="btnOrder">Volver atrás</button>
+        <button class="btnOrder">Enviar pedido</button>
+      </div>
     </div>
+
   )
 }
 
