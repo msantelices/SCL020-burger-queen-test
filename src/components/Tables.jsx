e039d6e697b8cab410c1c3be118395c65d224bdb
unknown <mariannybatista2003@gmail.com>
2022-08-08 11:00:44 -0400

    Rama pata traer cambios de Ale

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -1,48 +1,45 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 
 
-const Tables = ({history, setTableRegister}) => {
+const Tables = ({ history, setTableRegister }) => {
 
-    
+
 
     // useEffect(() => {
     //     localStorage.setItem("tableId", JSON.stringify(tableName));
     //   }, [tableName]);
-    
-
-
 
     const goToMenu = (e) => {
         const tableName = e
-    console.log(tableName)
-    setTableRegister(tableName)
+        console.log(tableName)
+        setTableRegister(tableName)
 
-            history.push('/menu')
+        history.push('/menu')
     }
 
-    
 
 
-  return (
-    <div className='containerOrder'>
+
+    return (
+        <div className='containerOrder'>
             <div className='textSelectTable'>Selecciona la mesa</div>
             <div className='containerCircles'>
-                <button type="button" className='circleTable boton-color' onClick={()=>goToMenu(1)} value='1' >
+                <button type="button" className='circleTable boton-color' onClick={() => goToMenu(1)} value='1' >
                     <div className='tableNumbers'>1</div>
                 </button>
-                <button type="button" className='circleTable boton-color'onClick={()=>goToMenu(2)} value='2'>
+                <button type="button" className='circleTable boton-color' onClick={() => goToMenu(2)} value='2'>
                     <div className='tableNumbers'>2</div>
                 </button>
-                <button type="button" className='circleTable boton-color' onClick={()=>goToMenu(3)} value='3'>
+                <button type="button" className='circleTable boton-color' onClick={() => goToMenu(3)} value='3'>
                     <div className='tableNumbers'>3</div>
                 </button>
-                <button type="button" className='circleTable boton-color' onClick={()=>goToMenu(4)} value='4'>
+                <button type="button" className='circleTable boton-color' onClick={() => goToMenu(4)} value='4'>
                     <div className='tableNumbers'>4</div>
                 </button>
             </div>
         </div>
-  )
+    )
 }
 
 export default withRouter(Tables)
